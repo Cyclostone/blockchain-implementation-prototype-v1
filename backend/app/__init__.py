88a19f7294b91a4c1728119ecb9c4f5a0135f444
Cyclostone <arpit.shrotriya5945@gmail.com>
2023-09-04 16:22:35 +0530

    Added Serialization Code and created transacttion_pool skeleton

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,57 +1,75 @@
 import os
 import random
 import requests
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from backend.blockchain.blockchain_mk7 import Blockchain
 from backend.pubsub_mk3 import PubSub
+from backend.wallet.wallet import Wallet
+from backend.wallet.transaction import Transaction
 
 app = Flask(__name__)
-# print(f'The NAME: {__name__}')
-
 blockchain = Blockchain()
 pubsub = PubSub(blockchain)
+wallet = Wallet()
 
 # for i in range(3):
 #     blockchain.addBlock(i)
 
-@app.route('/test')
+
+@app.route("/test")
 def test():
-    return 'test'
+    return "test"
 
-@app.route('/')
+
+@app.route("/")
 def route_default():
-    return 'Welcome to Blockchain'
+    return "Welcome to Blockchain"
 
-@app.route('/blockchain')
+
+@app.route("/blockchain")
 def route_blockchain():
     # return blockchain.chain
     # return blockchain.__repr__()
     return jsonify(blockchain.to_json())
 
-@app.route('/blockchain/mine')
+
+@app.route("/blockchain/mine")
 def route_blockchain_mine():
-    transaction_data = 'stubbed_transacton_data'
+    transaction_data = "stubbed_transacton_data"
     blockchain.addBlock(transaction_data)
     block = blockchain.chain[-1]
     pubsub.broadcast_block(block)
     return jsonify(block.to_json())
 
+
+@app.route("/wallet/transact", methods=["POST"])
+def route_wallet_transact():
+    # {'recipient':'foo', 'amount':15}
+    transaction_data = request.get_json()
+    transaction = Transaction(
+        wallet, transaction_data["recipient"], transaction_data["amount"]
+    )
+
+    print(f"transaction.to_json(): {transaction.to_json()}")
+
+    return jsonify(transaction.to_json())
+
+
 ROOT_PORT = 5000
 PORT = 5000
 
-if os.environ.get('PEER') == 'True':
+if os.environ.get("PEER") == "True":
     PORT = random.randint(5001, 6000)
 
-    result = requests.get(f'http://localhost:{ROOT_PORT}/blockchain')
-    print(f'result.json() : {result.json()}')
+    result = requests.get(f"http://localhost:{ROOT_PORT}/blockchain")
+    print(f"result.json() : {result.json()}")
 
     result_blockchain = Blockchain.from_json(result.json())
     try:
         blockchain.replace_chain(result_blockchain.chain)
-        print('\n -- Successfully synchronized the local chain')
+        print("\n -- Successfully synchronized the local chain")
     except Exception as e:
-        print(f'\n -- Error Synchronizing: {e}')
+        print(f"\n -- Error Synchronizing: {e}")
 
 # app.run(port=5001)
 app.run(port=PORT)
-
